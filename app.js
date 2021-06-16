818905a7a915aad2c4e56e1623bb60a547722c93
lettuce <lettuce.lu@lettucelu.com>
2021-06-17 00:46:20 +0800

    initialize project

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const handlebars = require('express-handlebars')
+
+app.engine('handlebars', handlebars())
+app.set('view engine', 'handlebars')
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
