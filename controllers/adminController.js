e282e5a4302c8474924b3f9adde2cb4a7b018a55
lettuce <lettuce.lu@lettucelu.com>
2021-06-22 21:39:48 +0800

    add category to adminController and view

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,16 +5,22 @@ const User = db.User
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
-
+const Category = db.Category
 
 
 
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true }).then(restaurants => {
-      return res.render('admin/restaurants', { restaurants: restaurants })
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
     })
+      .then(restaurants => {
+        // console.log(restaurants)
+        return res.render('admin/restaurants', { restaurants: restaurants })
+      })
   },
 
   createRestaurant: (req, res) => {
@@ -60,9 +66,10 @@ const adminController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/restaurant', { restaurant: restaurant })
-    })
+    return Restaurant.findByPk(req.params.id, { include: [Category] })
+      .then(restaurant => {
+        return res.render('admin/restaurant', { restaurant: restaurant.toJSON() })
+      })
   },
 
   editRestaurant: (req, res) => {
