c4c01d58c451d7a27accc30ca1cc0a0163812c69
lettuce <lettuce.lu@lettucelu.com>
2021-06-27 17:49:43 +0800

    feat: add followship model

--- config/passport.js
+++ config/passport.js
@@ -27,7 +27,9 @@ passport.serializeUser((user, cb) => {
 passport.deserializeUser((id, cb) => {
   User.findByPk(id, {
     include: [
-      { model: Restaurant, as: 'FavoritedRestaurants' }
+      { model: Restaurant, as: 'FavoritedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
     ]
   }).then(user => {
     user = user.toJSON() // 此處與影片示範不同
